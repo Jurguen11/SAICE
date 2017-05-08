1d4716309f32de07967e36b69a9aeb731fef8a23
Andresf12 <landresf3638@gmail.com>
2017-05-07 18:01:28 -0600

    Query base de datos version 1.0

---
+++ SAICE.sql
@@ -0,0 +1,156 @@
+﻿--drop table Eventos,Personas,Funcionarios,EF,Correos_p;
+--drop table Telefonos_p;
+CREATE DOMAIN a_telefono CHAR(9) NOT NULL CONSTRAINT CHK_telefono
+CHECK (VALUE SIMILAR TO '[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]');
+
+CREATE DOMAIN a_correos CHAR(30) NOT NULL CONSTRAINT CHK_correo
+CHECK(VALUE SIMILAR TO '[A-z]%@[A-z]%.[A-z]%');
+
+CREATE DOMAIN a_cedula CHAR(9)NOT NULL CONSTRAINT CHK_cedula 
+CHECK(VALUE SIMILAR TO '[0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9]');
+
+CREATE DOMAIN a_carnet CHAR(11) NOT NULL CONSTRAINT CHK_carnet 
+CHECK (VALUE SIMILAR TO '[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9][0-9][0-9]');
+
+
+ CREATE TABLE Eventos(
+	ID_Evento CHAR(7) primary key NOT NULL,
+	Nombre VARCHAR(30) NOT NULL,
+	Descripcion VARCHAR(100) NULL,
+	FechaInicio DATE NOT NULL,
+	FechaFinal DATE NOT NULL
+                    );
+
+ CREATE TABLE Personas(
+		cedula a_cedula primary key ,
+		Nombre CHAR(30) NOT NULL,
+		Apellido1 CHAR(30) NOT NULL,
+		Apellido2 CHAR(30) NULL,
+		provincia VARCHAR(30) NOT NULL,
+		canton VARCHAR(30) NOT NULL,
+		distrito VARCHAR(30) NOT NULL,
+		detalle VARCHAR(100) NULL
+                      );
+CREATE TABLE Funcionarios(
+		cedula a_cedula primary key ,
+		carnet char(10) NOT NULL,
+		CONSTRAINT FK_cedula_personas_funcionarios FOREIGN KEY (cedula) REFERENCES Personas
+                         );
+CREATE TABLE EF(
+	cedula a_cedula ,
+	ID_Evento CHAR(7) NOT NULL,
+	ROL VARCHAR(30) NOT NULL,
+        CONSTRAINT PK_funcionarios_eventos PRIMARY KEY(cedula,ID_evento), 
+	CONSTRAINT FK_cedula_Funcionarios_eventos FOREIGN KEY(cedula) REFERENCES Funcionarios,
+	CONSTRAINT FK_idEvento_Funcionarios FOREIGN KEY (ID_Evento) REFERENCES Eventos
+	
+	
+               );
+CREATE TABLE Correos_p(
+	cedula a_cedula PRIMARY KEY,
+	correo a_correos ,
+	CONSTRAINT FK_cedula_personas_correos FOREIGN KEY(cedula) REFERENCES Personas
+                      ); 
+CREATE TABLE Telefonos_p(
+	cedula a_cedula primary key,
+	telefono a_telefono,
+	CONSTRAINT FK_cedula_persona_telefonos FOREIGN KEY(cedula) REFERENCES Personas
+                        );
+CREATE TABLE Secciones(
+	Id_secciones char(7) NOT NULL PRIMARY KEY
+		      );
+CREATE TABLE SF(
+	Id_secciones char(7) NOT NULL ,
+	cedula a_cedula,
+	CONSTRAINT PK_funcionario_secciones PRIMARY KEY (Id_secciones,cedula),
+	CONSTRAINT FK_id_secciones_funcionarios_secciones FOREIGN KEY(Id_secciones)REFERENCES Secciones,
+	CONSTRAINT FK_cedula_funcionarios_secciones FOREIGN KEY(cedula) REFERENCES Funcionarios 
+               ); 
+CREATE TABLE Polizas(
+	ID_Poliza CHAR(7) NOT NULL PRIMARY KEY,
+	Descripcion VARCHAR(100) NULL,
+	Monto INT NOT NULL,
+	Fecha_vencimiento DATE NOT NULL,
+	Aseguradora VARCHAR(30) NOT NULL
+                    );
+CREATE TABLE Estudiantes(
+	cedula a_cedula PRIMARY KEY,
+	ID_Poliza CHAR(7) NOT NULL,
+	CONSTRAINT FK_polizas_estudiantes FOREIGN KEY(ID_Poliza) REFERENCES Polizas	
+                        );
+CREATE TABLE Contactos(
+	cedula a_cedula PRIMARY KEY ,
+	Puesto VARCHAR(30) NOT NULL,
+	CONSTRAINT FK_cedula_estudiantes_contactos FOREIGN KEY(cedula)REFERENCES Estudiantes
+                      );
+CREATE TABLE Empresas(
+	ID_Empresa CHAR(7) NOT NULL PRIMARY KEY,
+	Nombre CHAR(30) NOT NULL,
+	provincia VARCHAR(30) NOT NULL,
+	canton VARCHAR(30) NOT NULL,
+	distrito VARCHAR(30) NOT NULL,
+	detalle VARCHAR(100) NULL
+                    );
+CREATE TABLE CE(
+	cedula a_cedula,
+	ID_Empresa CHAR(7) NOT NULL,
+	CONSTRAINT PK_Contactos_de_empresa PRIMARY KEY(cedula,ID_Empresa),
+	CONSTRAINT FK_cedula_contacto_empresa FOREIGN KEY(cedula)REFERENCES Contactos,
+	CONSTRAINT FK_id_empresa_de_contacto FOREIGN KEY(ID_Empresa)REFERENCES Empresas
+               );                    
+CREATE TABLE Telefonos_E(
+		ID_Empresa CHAR(7) NOT NULL PRIMARY KEY,
+		telefono a_telefono,
+		CONSTRAINT FK_id_empresa_telefonos FOREIGN KEY(ID_Empresa) REFERENCES Empresas
+                        );
+CREATE TABLE Correos_E(
+	ID_Empresa CHAR(7) NOT NULL PRIMARY KEY,
+	correo a_correos ,
+	CONSTRAINT FK_id_empresa_correos FOREIGN KEY(ID_Empresa) REFERENCES Empresas
+                );
+CREATE TABLE PRACTICAS(
+		ID_Practicas VARCHAR(7) NOT NULL PRIMARY KEY,
+		Fecha_inicio DATE NOT NULL,
+		Fecha_final DATE NOT NULL,
+		nota int  NOT NULL,
+		estado CHAR(1) NOT NULL,
+		cedula a_cedula,
+		ID_Empresa CHAR(7) NOT NULL,
+		CONSTRAINT FK_id_empresa_practicas FOREIGN KEY(ID_Empresa) REFERENCES Empresas,
+		CONSTRAINT FK_cedula_estudiante_Practicas FOREIGN KEY(cedula) REFERENCES Estudiantes
+                      );
+CREATE TABLE Giras(
+	ID_Gira CHAR(7) NOT NULL PRIMARY KEY,
+	Fecha_inicio DATE NOT NULL,
+	Fecha_final DATE NOT NULL,
+	costo INT NOT NULL,
+	duracion TIME NOT NULL,
+	provincia VARCHAR(30) NOT NULL,
+	canton VARCHAR(30) NOT NULL,
+	distrito VARCHAR(30) NOT NULL,
+	detalle VARCHAR(100) NULL
+                  );
+CREATE TABLE SG(
+	Id_secciones CHAR(7) NOT NULL,
+	ID_Gira CHAR(7) NOT NULL,
+	CONSTRAINT PK_Secciones_Giras PRIMARY KEY(Id_secciones,ID_Gira),
+	CONSTRAINT FK_id_secciones_giras FOREIGN KEY(Id_secciones)REFERENCES Secciones,
+	CONSTRAINT FK_id_giras_secciones FOREIGN KEY(ID_Gira)REFERENCES Giras
+
+               );
+CREATE TABLE ES(
+	Id_secciones CHAR(7) NOT NULL,
+	cedula a_cedula,
+	CONSTRAINT PK_Estudiantes_secciones PRIMARY KEY(Id_secciones,cedula),
+	CONSTRAINT FK_Id_secciones_Estudiantes FOREIGN KEY(Id_secciones) REFERENCES Secciones,
+	CONSTRAINT FK_cedula_estudiantes_secciones FOREIGN KEY(cedula)REFERENCES Estudiantes
+
+               );
+CREATE TABLE  GE(
+	ID_Giras CHAR(7) NOT NULL,
+	ID_Empresa CHAR(7) NOT NULL,
+	CONSTRAINT PK_Giras_empresas PRIMARY KEY(ID_Giras,ID_Empresa),
+	CONSTRAINT FK_ID_empresa_Giras FOREIGN KEY(ID_Empresa)REFERENCES Empresas,
+	CONSTRAINT FK_ID_giras_empresas FOREIGN KEY(ID_Giras)REFERENCES Giras
+               ); 
+         
